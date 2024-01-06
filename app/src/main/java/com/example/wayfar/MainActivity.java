245fcae30d59e943644ff04a54a4dbfe39af7c3f
Rajsee Panwar <17rashipanwar@gmail.com>
2024-01-06 14:36:46 +0530

    first commit

--- app/src/main/java/com/example/wayfar/MainActivity.java
+++ app/src/main/java/com/example/wayfar/MainActivity.java
@@ -1,14 +1,105 @@
 package com.example.wayfar;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawer_layout;
+
+    NavigationView navigation_view;
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawer_layout = findViewById(R.id.drawer_layout);
+        navigation_view = findViewById(R.id.navigationView);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        // isme 5 values paas hoti hain hmesha
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer_layout, R.string.open_drawer, R.string.close_drawer);
+
+        drawer_layout.addDrawerListener(toggle);
+
+        toggle.syncState();
+
+        // home vala fragment ye by default open hojayga... baki sab isko replace krenge
+        loadFragment(new HomeFragment());
+
+        navigation_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+
+                if (id==R.id.home) {
+                    loadFragment(new HomeFragment());
+                } else if (id==R.id.basukedar) {
+                    //loadFragment(new BasukedarFragment());
+                    Toast.makeText(MainActivity.this, "Basukedar Temple", Toast.LENGTH_SHORT).show();
+                } else if (id==R.id.someshwar) {
+                    Toast.makeText(MainActivity.this, "Someshwar Temple", Toast.LENGTH_SHORT).show();
+                } else if (id==R.id.mata_man) {
+                    Toast.makeText(MainActivity.this, "Mata Man Ichha Devi Temple", Toast.LENGTH_SHORT).show();
+                } else if (id==R.id.bansi_narayan) {
+                    Toast.makeText(MainActivity.this, "Bansi Narayan Temple", Toast.LENGTH_SHORT).show();
+                } else if (id==R.id.badrinath) {
+                    Toast.makeText(MainActivity.this, "Badrinath Temple", Toast.LENGTH_SHORT).show();
+
+                } else if (id==R.id.kedarnath) {
+                    Toast.makeText(MainActivity.this, "Kedarnath Temple", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Flower Valley", Toast.LENGTH_SHORT).show();
+                }
+
+                drawer_layout.closeDrawer(GravityCompat.START);
+
+                return true;
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        ft.add(R.id.container, fragment);
+        ft.commit();
+
+
     }
 }
