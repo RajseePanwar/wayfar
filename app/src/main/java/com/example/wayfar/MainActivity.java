052d82db266457b951fa8e204dff6c6c9f50d9bc
lynchtiwari <bt21cse005@nituk.ac.in>
2024-01-08 14:03:08 +0530

    start

--- app/src/main/java/com/example/wayfar/MainActivity.java
+++ app/src/main/java/com/example/wayfar/MainActivity.java
@@ -11,10 +11,12 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
@@ -27,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
 
         setSupportActionBar(toolbar);
-
+        toolbar.setTitleTextColor(Color.WHITE);
         // isme 5 values paas hoti hain hmesha
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.open_drawer, R.string.close_drawer);
@@ -54,22 +57,29 @@ public class MainActivity extends AppCompatActivity {
                 int id = item.getItemId();
 
                 if (id==R.id.dashboard) {
+                    toolbar.setTitle("Dashboard");
                     Toast.makeText(MainActivity.this, "Dashboard", Toast.LENGTH_SHORT).show();
                 } else if (id==R.id.basukedar) {
-
+                    toolbar.setTitle("Basukedar Temple");
                     Toast.makeText(MainActivity.this, "Basukedar Temple", Toast.LENGTH_SHORT).show();
                 } else if (id==R.id.someshwar) {
+                    toolbar.setTitle("Someshwar Temple");
                     Toast.makeText(MainActivity.this, "Someshwar Temple", Toast.LENGTH_SHORT).show();
                 } else if (id==R.id.mata_man) {
+                    toolbar.setTitle("Mata Man Ichha Devi Temple");
                     Toast.makeText(MainActivity.this, "Mata Man Ichha Devi Temple", Toast.LENGTH_SHORT).show();
                 } else if (id==R.id.bansi_narayan) {
+                    toolbar.setTitle("Bansi Narayan Temple");
                     Toast.makeText(MainActivity.this, "Bansi Narayan Temple", Toast.LENGTH_SHORT).show();
                 } else if (id==R.id.badrinath) {
+                    toolbar.setTitle("Badrinath Temple");
                     Toast.makeText(MainActivity.this, "Badrinath Temple", Toast.LENGTH_SHORT).show();
 
                 } else if (id==R.id.kedarnath) {
+                    toolbar.setTitle("Kedarnath Temple");
                     Toast.makeText(MainActivity.this, "Kedarnath Temple", Toast.LENGTH_SHORT).show();
-                } else {
+                } else if(id==R.id.flower_valley){
+                    toolbar.setTitle("Valley of Flowers");
                     Toast.makeText(MainActivity.this, "Flower Valley", Toast.LENGTH_SHORT).show();
                 }
 
